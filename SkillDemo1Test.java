b0cdae1c86eacbf985881dedaffcf7cf07421e63
KunWang0129 <103203141+KunWang0129@users.noreply.github.com>
2022-04-30 22:11:10 -0700

    Update SkillDemo1Test.java

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemo1Test {
     @Test 
     public void multiplication(){
-        assertEquals(10, SkillDemo1.multiply(2,3));
+        assertEquals(10, SkillDemo1.multiply(2,5));
     }
 }
