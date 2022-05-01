2f68aba3035776faa7e64d8c8c5120ca57247d99
KunWang0129 <103203141+KunWang0129@users.noreply.github.com>
2022-04-30 22:08:26 -0700

    Create SkillDemo1Test.java

---
+++ SkillDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+public class SkillDemo1Test {
+    @Test 
+    public void multiplication(){
+        assertEquals(10, SkillDemo1.multiply(2,3));
+    }
+}
